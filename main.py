8e3e461218f5d1142ca73e2ea1fc7865c9b68108
Maktub108 <e.kozulin@yandex.ru>
2025-03-22 18:32:55 +0500

    add cycle  code

--- main.py
+++ main.py
@@ -11,14 +11,40 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/87c70fb.jpeg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/23e6290e855d832671f7a9a4c9d35039.jpg")
+target_img = pygame.image.load("img/icons8-яблоко-64.png")
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0,255),random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+hit_count = 0
+
+font = pygame.font.Font(None, 36)
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                hit_count += 1
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+
+    hit_text = font.render(f"Количество попаданий в цель: {hit_count}", True, (255, 255, 255))
+    screen.blit(hit_text, (10, 10))
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+
+pygame.quit()
+
+
+
